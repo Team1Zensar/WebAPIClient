f8a1ea2a5af23a5b057244d29704bee605dc8338
mavurigayatri <mavurigayatri2020@gmail.com>
2021-01-12 13:24:58 +0530

    Add project files.

---
+++ WebAPIClient/Controllers/JqueryDemoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPIClient.Controllers
+{
+    public class JqueryDemoController : Controller
+    {
+        // GET: JqueryDemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+
